e35bd25fccf13839d62dbfb4efa89453386be21b
Desablero <desablero@gmail.com>
2020-10-12 16:03:36 +0300

    3\35 Вставить "-" между словами строки (два способа)

--- strings.js
+++ strings.js
@@ -12,7 +12,7 @@ console.log(stringToarray(str))
 
 // 2\35 Напишите функцию delete_characters(str, length), которая возвращает подстроку,
 // состоящую из указанного количества символов
-
+/*
 var str = 'Каждый охотник желает знать';
 function delete_characters(str, length) {
     return str = str.split('').slice(0, length).join('')
@@ -20,3 +20,17 @@ function delete_characters(str, length) {
 console.log(delete_characters(str,14))
 // Метод из ответа создаёт дополнительные проверки валидности строки: if ((str.constructor === String) && (length>0)) {
 // return str.slice(0, length);
+*/
+
+// 3\35 Напишите функцию insert_dash(str), которая принимает строку str в качестве аргумента и вставляет тире (-)
+// между словами. При этом все символы строки необходимо перевести в верхний регистр.
+
+var str = "HTML JavaScript PHP";
+function insert_dash(str) {
+    // return str.trim().toUpperCase().split(' ').join('-')
+    return str.trim().toUpperCase().replace(/[^a-zA-Z0-9 -]/, "").replace(/\s/g, "-");
+}
+console.log(insert_dash(str))
+// Метод из ответа: return str.trim().toUpperCase().replace(/[^a-zA-Z0-9 -]/, "").replace(/\s/g, "-");
+// replace(/\s/g, "-"); заменяет пробелы на "-"
+
