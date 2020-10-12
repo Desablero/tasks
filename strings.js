f1c33c17564b6c53120c9a2e591bf4193cc3ad21
Desablero <desablero@gmail.com>
2020-10-12 14:41:44 +0300

    Create strings.js
    Решения задач по строкам

---
+++ strings.js
@@ -0,0 +1,14 @@
+// Link: http://wm-school.ru/js/string_exercises.php
+
+// 1\35 Напишите функцию stringToarray(str), которая преобразует строку в массив слов
+
+/*
+var str = 'Каждый охотник желает знать';
+function stringToarray(str) {
+
+// Ваш код
+
+};
+
+var arr = stringToarray(str);
+*/
