73622a23e02118241f3753221765cbb66d3a4fd1
Desablero <desablero@gmail.com>
2020-10-12 15:06:01 +0300

    1\35 Преобразование строки в массив слов

--- strings.js
+++ strings.js
@@ -3,7 +3,8 @@
 // 1\35 Напишите функцию stringToarray(str), которая преобразует строку в массив слов
 
 var str = 'Каждый охотник желает знать';
-function stringToarray(str) {
+function stringToarray(str){
     return str.split(' ') // если убрать пробел в кавычках, тогда массив будет из букв, а не из слов
+    // метод из ответа: str.trim().split(" "); | если в строке будет вначале\вконце пробел, тогда он станет элем. масс.
 }
 console.log(stringToarray(str))
